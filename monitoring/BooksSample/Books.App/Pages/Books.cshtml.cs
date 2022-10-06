d2b8de8d76a6cf9b09de721a9be6f51cd45c2b6b
Christian Nagel <christian@christiannagel.com>
2022-10-06 21:05:40 +0200

    C# syntax enhancements

--- monitoring/BooksSample/Books.App/Pages/Books.cshtml.cs
+++ monitoring/BooksSample/Books.App/Pages/Books.cshtml.cs
@@ -1,17 +1,16 @@
 ﻿using Books.Shared;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Threading.Tasks;
 
-namespace Books.App.Pages
+namespace Books.App.Pages;
+
+public class BooksModel : PageModel
 {
-    public class BooksModel : PageModel
-    {
-        public Book[]? Books { get; set; }
+    public Book[]? Books { get; set; }
 
-        public async Task OnGet([FromServices] BooksClient booksClient)
-        {
-            Books = await booksClient.GetBooksAsync();
-        }
+    public async Task OnGet([FromServices] BooksClient booksClient)
+    {
+        Books = await booksClient.GetBooksAsync();
     }
 }
