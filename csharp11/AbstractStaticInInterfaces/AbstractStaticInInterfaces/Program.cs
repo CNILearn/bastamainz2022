869d91a08bec014bd96f2d22b5613e394d628f04
Christian Nagel <christian@christiannagel.com>
2022-10-09 22:24:04 +0200

    abstract static members

---
+++ csharp11/AbstractStaticInInterfaces/AbstractStaticInInterfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Test<FooTest> t1 = new();
+t1.TestMethod();
+
+public class FooTest : IFoo
+{
+    public static void Foo()
+    {
+        Console.WriteLine("FooTest.Foo");
+    }
+}
+
+public class Test<T>
+    where T : IFoo
+{
+    public void TestMethod()
+    {
+        T.Foo();
+    }
+}
+
+public interface IFoo
+{
+    abstract static void Foo();
+}
