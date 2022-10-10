bcec4a8867026f40ccdaa4432b3e63bdd2aaba57
Christian Nagel <christian@christiannagel.com>
2022-10-10 15:14:05 +0200

    igrnorecase with GeneratedRegex

--- csharp11/06_SourceGenerators/RegExSample/Program.cs
+++ csharp11/06_SourceGenerators/RegExSample/Program.cs
@@ -1,10 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-string email = "christian@christiannagel.com";
+string email = "Christian@christiannagel.com";
 bool isEmail = MyRegEx.CheckEmail(email);
 Console.WriteLine(isEmail);
 
@@ -13,7 +10,7 @@ Console.WriteLine(isEmail);
 
 public partial class MyRegEx
 {
-    [GeneratedRegex(@"^[a-z|A-Z]+@[a-z|A-Z]+.[a-z|A-Z]+$")]
+    [GeneratedRegex("""^[a-z]+@[a-z]+.[a-z]+$""", RegexOptions.IgnoreCase)]
     public static partial Regex Email();
 
     public static bool CheckEmail(string input)
