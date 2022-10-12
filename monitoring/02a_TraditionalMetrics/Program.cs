ed4ca214df8ecb3af1d16fedee6fd8e1b761aa52
Christian Nagel <christian@christiannagel.com>
2022-10-12 14:10:26 +0200

    add traditional metrics api sample

---
+++ monitoring/02a_TraditionalMetrics/Program.cs
@@ -0,0 +1,62 @@
+﻿using MetricsSample;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using System.Runtime.InteropServices;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        logging.ClearProviders();
+        bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        if (isWindows)
+        {
+            // Default the EventLogLoggerProvider to warning or above
+            logging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning);
+        }
+
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        logging.AddDebug();
+        logging.AddEventSourceLogger();
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            logging.AddEventLog(); // EventLogLoggerProvider
+        }
+        // logging.AddConsole();
+        //logging.AddSimpleConsole(config =>
+        //{
+        //    config.IncludeScopes = true;
+        //});
+        //logging.AddSystemdConsole(configure =>
+        //{
+        //    configure.IncludeScopes = true;
+        //});
+        //logging.AddJsonConsole(configure =>
+        //{
+        //    configure.IncludeScopes = true;
+        //});
+
+
+        logging.Configure(options =>
+        {
+            options.ActivityTrackingOptions = ActivityTrackingOptions.SpanId
+                                                | ActivityTrackingOptions.TraceId
+                                                | ActivityTrackingOptions.ParentId;
+        });
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient<NetworkService>(client =>
+        {
+        }).AddTypedClient<NetworkService>();
+        services.AddScoped<Runner>();
+    }).Build();
+
+var runner = host.Services.GetRequiredService<Runner>();
+await runner.RunAsync();
+
+Console.WriteLine("Bye... Press return to exit");
+Console.ReadLine();
---
+++ monitoring/02a_TraditionalMetrics/Runner.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MetricsSample;
+
+class Runner
+{
+    private readonly ILogger _logger;
+    private readonly NetworkService _networkSevice;
+    public Runner(NetworkService networkService, ILogger<Runner> logger)
+    {
+        _networkSevice = networkService;
+        _logger = logger;
+    }
+
+    public async Task RunAsync()
+    {
+        _logger.LogDebug("RunAsync started");
+        bool exit = false;
+        do
+        {
+            Console.Write("Please enter a URI or 'exit' to exit: ");
+            string url = Console.ReadLine() ?? throw new InvalidOperationException("null returned from Console.ReadLine");
+            using var _ = _logger.BeginScope("RunAsync iteration, url: {url}", url);
+            if (url.ToLower() != "exit")
+            {
+                try
+                {
+                    Uri uri = new(url);
+                    await _networkSevice.NetworkRequestSampleAsync(uri);
+                }
+                catch (UriFormatException ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                }
+            }
+            else
+            {
+                exit = true;
+            }
+        } while (!exit);
+    }
+}
